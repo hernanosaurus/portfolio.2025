ae65eafb827b0b5867dc6f61430242b83eaa65f7
hernanosaurus <hterania.dev@gmail.com>
2025-11-22 00:57:02 +0800

    changes to layout to use new favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,9 @@ export default function RootLayout({
           name="description"
           content="Nani Terania (hernanosaurus) – Frontend Engineer creating clean, modern, and accessible web experiences."
         />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="icon" type="image/png" sizes="64x64" href="/favicon-64x64.png" />
+        <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48x48.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
